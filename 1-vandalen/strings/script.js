36b642b258d4dc5bee2a6b40d9be6832d89aad9c
kh222mj <kh222mj@student.lnu.se>
2014-11-11 17:59:11 +0000

    uppgift 2 klar

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -7,13 +7,25 @@ window.onload = function(){
 		// Plats för förändring.		
 		// Returnera den konverterade strängen.
 		// Vid fel, kasta ett undantag med ett meddelande till användaren. 
-	
 
-
-
-
-
-
+			if(str.length === 0 || (/^\s*$/).test(str))
+			{
+				return "Skriv in en text du vill formatera!";
+			}
+			str = str.replace(/A/gi, "#");
+			var omv = "";
+			for (var i = 0; i < str.length; i++) 
+			{
+	            if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90 || str.charCodeAt(i) >= 192 && str.charCodeAt(i) <= 214) 
+	            {
+	                omv = omv + str.charAt(i).toLowerCase();
+	            }
+	            else
+	            {
+	            	omv = omv + str.charAt(i).toUpperCase();	
+	            }
+		    }
+			return omv;	
 	};
 	// ------------------------------------------------------------------------------
 
