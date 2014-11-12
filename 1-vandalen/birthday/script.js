4fa13da3eb3fb68a2f44cd99479839fea69db928
kh222mj <kh222mj@student.lnu.se>
2014-11-12 14:44:59 +0000

    uppgift 3 klar

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -1,17 +1,57 @@
 "use strict";
 
 window.onload = function(){
-
 	
-	var birthday = function(date){
-		
-
-
-			// Din kod här.
-
-
-
-
+			var birthday = function(date){
+			var month, day;
+			var regPat = /[0-9]{4}\-[0-9]{2}\-[0-9]{2}/;
+			
+		try
+		{
+			if(regPat.test(date))
+			{
+				if(date.charAt(5) === 0)
+				{
+					month = date.charAt(6);
+				}
+				else
+				{
+					month = date.charAt(5) + date.charAt(6) - 1;	
+				}
+				if(date.charAt(8) === 0)
+				{
+					day = date.charAt(9);
+				}
+				else
+				{
+					day = date.charAt(8) + date.charAt(9);
+				}
+				var birthday = new Date();
+				birthday.setDate(day);
+				birthday.setMonth(month);
+				
+				var currentdate = new Date();
+				if(currentdate.getTime() > birthday.getTime()) 
+				{
+			    	birthday.setFullYear(birthday.getFullYear() + 1);
+				} 
+				birthday = birthday.getTime();
+				
+				currentdate = currentdate.getTime();
+				var theDate = birthday - currentdate;
+				theDate = Math.floor(theDate/(1000 * 60 * 60 * 24));
+			
+				return theDate;
+			}
+			else 
+			{
+				throw new Error("Formateringen är inte korrekt!");
+			}
+		}
+		catch(error)
+		{
+			alert(error);
+		}
 	};
 	// ------------------------------------------------------------------------------
 
