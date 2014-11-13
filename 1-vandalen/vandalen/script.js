dd073c87a8b35f23d4c642168265b16c012fc8a2
kh222mj <kh222mj@student.lnu.se>
2014-11-13 17:35:40 +0000

    niva 1 avklarad

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,37 @@
 "use strict";
 
 var makePerson = function(persArr){
-
-
-	// Din kod här...
-
+    
+    //Läser in object till arrayer
+    var arrN = [];
+    var arrA = [];
+    for(var i = 0; i < persArr.length;)
+    {
+        arrN.push(persArr[i].name);
+        i++;
+    }
+    
+    for(var i = 0; i < persArr.length;)
+    {
+        arrA.push(persArr[i].age);
+        i++;
+    }
+    // Formaterar namn
+    arrN = arrN.sort(String.localeCompare);
+    var names = arrN.join(", ");
+    
+    // Beräknar medelvärde
+    var averageAge = arrA.reduce(function(a,b){ return a+b; }) / arrA.length;
+    averageAge = Math.round(averageAge);
+    
+    // Sortering nummer
+    arrA.sort(function(a, b){return a-b});
+    
+    var minAge = arrA[0];
+    var maxAge = arrA[arrA.length - 1];
+    
+    // Returning
+    var persNy = { minAge:minAge, maxAge:maxAge, averageAge:averageAge, names:names };
+    
+    return persNy;
 }
-
